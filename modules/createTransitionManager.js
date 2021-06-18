c2910f87feb542b385003b6344eeb1dd609ee29e
yunfengxu <yunfengxu@microsoft.com>
2021-06-18 16:29:32 +0800

    chore: drop prompt

--- modules/createTransitionManager.js
+++ modules/createTransitionManager.js
@@ -1,17 +1,4 @@
-import warning from './warning.js';
-
 function createTransitionManager() {
-  let prompt = null;
-
-  function setPrompt(nextPrompt) {
-    warning(prompt == null, 'A history supports only one prompt at a time');
-
-    prompt = nextPrompt;
-
-    return () => {
-      if (prompt === nextPrompt) prompt = null;
-    };
-  }
 
   function confirmTransitionTo(
     location,
@@ -19,31 +6,9 @@ function createTransitionManager() {
     getUserConfirmation,
     callback
   ) {
-    // TODO: If another transition starts while we're still confirming
     // the previous one, we may end up in a weird state. Figure out the
     // best way to handle this.
-    if (prompt != null) {
-      const result =
-        typeof prompt === 'function' ? prompt(location, action) : prompt;
-
-      if (typeof result === 'string') {
-        if (typeof getUserConfirmation === 'function') {
-          getUserConfirmation(result, callback);
-        } else {
-          warning(
-            false,
-            'A history needs a getUserConfirmation function in order to use a prompt message'
-          );
-
-          callback(true);
-        }
-      } else {
-        // Return false from a transition hook to cancel the transition.
-        callback(result !== false);
-      }
-    } else {
-      callback(true);
-    }
+    callback(true);
   }
 
   let listeners = [];
@@ -68,7 +33,6 @@ function createTransitionManager() {
   }
 
   return {
-    setPrompt,
     confirmTransitionTo,
     appendListener,
     notifyListeners
